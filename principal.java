9d6c4b7ad7dfe2445068b455c67b120227a3268c
pokB01 <pokB01@x.y.z>
2023-03-07 14:20:37 +0100

    Creados 3 puntos

---
+++ principal.java
@@ -0,0 +1,9 @@
+
+public class principal {
+    public static void main(String[] args) {
+        Punto p1 = new Punto();
+        Punto p2 = new Punto();
+        Punto p3 = new Punto();
+        
+    }
+}
