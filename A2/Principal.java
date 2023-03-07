80e63f85a2c5ba992b742fec1993c8345990c08a
pokB01 <pokB01@x.y.z>
2023-03-07 14:45:31 +0100

    Personas y scanner

--- A2/Principal.java
+++ A2/Principal.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+        Scanner Lector = new Scanner(System.in);
+        Persona p1 = new Persona();
+        Persona p2 = new Persona();
+        String nombre,dni,apellido;
+        int edad;
         
     }
 }
