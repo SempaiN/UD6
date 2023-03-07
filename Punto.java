b0b12e1f231512a630399c09a42745574a7736a4
pokB01 <pokB01@x.y.z>
2023-03-07 14:20:19 +0100

    Creado punto y métodos

---
+++ Punto.java
@@ -0,0 +1,14 @@
+/**
+ * Punto
+ */
+
+public class Punto {
+    private int x,y;
+    
+    public void ponX(int corx){
+        x = corx;
+    }
+    public void ponY(int cory){
+        y = cory;
+    }
+}
