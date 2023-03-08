1e29add5577cac57a3d6862ac4558c4b1c46aecc
Sempain <trollnacho.np@gmail.com>
2023-03-08 19:07:21 +0100

    Nuevo Réctangulo

--- A3/Principal.java
+++ A3/Principal.java
@@ -8,7 +8,15 @@ public class Principal {
         r1.setX2(5);
         r1.setY1(0);
         r1.setY2(5);
+
         System.out.println("El area de R1 es " + r1.calcArea());
-        System.out.println("El perimetro de R1 es " + r1.calcPerimetro());
+        System.out.println("El perimetro de R1 es " + r1.calcPerimetro());r1.setX1(0);
+
+        r2.setX2(5);
+        r2.setY1(0);
+        r2.setY2(5);
+
+        System.out.println("El area de R2 es " + r2.calcArea());
+        System.out.println("El perimetro de R2 es " + r2.calcPerimetro());
     }
 }
