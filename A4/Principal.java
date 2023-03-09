5e3b3a6331d233f76c32f33d998e692917cdff5c
Sempain <trollnacho.np@gmail.com>
2023-03-09 19:40:09 +0100

    Pideprecio y pidenombre en el main

--- A4/Principal.java
+++ A4/Principal.java
@@ -7,6 +7,15 @@ public class Principal {
     public static void main(String[] args) {
         
     }
-    
+    public static String pideNombre(){
+        System.out.println("Dame el nombre del producto");
+        String nombre = lector.next();
+        return nombre;
+    }
+    public static int pidePrecio(){
+        System.out.println("Dame el precio del producto");
+        int precio = lector.nextInt();
+        return precio;
+    }
 
 }
