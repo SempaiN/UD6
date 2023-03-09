5d161403ced02ffcb36ce99ae6d3328f1c8a0e12
Sempain <trollnacho.np@gmail.com>
2023-03-09 19:37:00 +0100

    Scanner en main

--- A4/Principal.java
+++ A4/Principal.java
@@ -1,5 +1,12 @@
 package A4;
 
+import java.util.Scanner;
+
 public class Principal {
+    public static Scanner lector = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+    }
     
+
 }
