384bf4ea5ce7d76ff8aaa40cd34aa026ca7c3ea9
pokB01 <pokB01@x.y.z>
2023-03-10 12:52:15 +0100

    Creadas cosas de Punto

--- A1/Principal.java
+++ A1/Principal.java
@@ -1,3 +1,18 @@
 public class Principal {
-    
+    public static void main(String[] args) {
+        Punto P1 = new Punto();
+        Punto P2 = new Punto();
+        Punto P3 = new Punto();
+        
+        P1.x=5;
+        P1.y=0;
+        
+        P2.x=10;
+        P2.y=10;
+        
+        P3.x=-3;
+        P3.y=7;
+
+        System.out.println("("+P1.x+","+P1.y+")");
+    }
 }
