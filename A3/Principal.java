a0f12bfd81f8a47a217ec24fbee19d788f479a9e
pokB01 <pokB01@x.y.z>
2023-03-10 13:33:05 +0100

    r1 y r2

--- A3/Principal.java
+++ A3/Principal.java
@@ -2,6 +2,18 @@ package A3;
 
 public class Principal {
     public static void main(String[] args) {
+        Rectangulo r1 = new Rectangulo();
+        Rectangulo r2 = new Rectangulo();
+        int altura,base,perimetro,area;
+        r1.x1=0;
+        r1.y1=0;
+        r1.x2=5;
+        r1.y2=5;
+
+        r2.x1=7;
+        r2.y1=9;
+        r2.x2=2;
+        r2.y2=3;
         
     }
 }
