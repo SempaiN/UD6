1632b0e7e076997a36fa00f39522e82315e046a5
pokB01 <pokB01@x.y.z>
2023-03-10 12:19:16 +0100

    Copiado

--- B1/Punto.java
+++ B1/Punto.java
@@ -1,5 +1,22 @@
 package B1;
 
-public class Punto {
+/**
+ * Punto
+ */
+
+ public class Punto {
+    private int x,y;
     
+    public void setX(int corx){
+        x = corx;
+    }
+    public void setY(int cory){
+        y = cory;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
 }
