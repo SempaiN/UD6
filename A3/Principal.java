e60b2d3ba2f2d3f284d696d4868f92e0b279d9c9
pokB01 <pokB01@x.y.z>
2023-03-10 13:44:42 +0100

    Funcionalidad completa

--- A3/Principal.java
+++ A3/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
         Rectangulo r1 = new Rectangulo();
         Rectangulo r2 = new Rectangulo();
         int altura,base,perimetro,area;
+
         r1.x1=0;
         r1.y1=0;
         r1.x2=5;
@@ -14,6 +15,19 @@ public class Principal {
         r2.y1=9;
         r2.x2=2;
         r2.y2=3;
+
+        altura = r1.y2-r1.y1;
+        base = r1.x2-r1.x1;
+        perimetro = 2*base+2*altura;
+        area = altura*base;
+        System.out.println("(" + r1.x1+"," + r1.y1+")" + "("+r1.x2+","+r1.y2+")" + " el périmetro es " + perimetro + " el area es " + area);
         
+        System.out.println();
+ 
+        altura = r2.y1-r2.y2;
+        base = r2.x1-r2.x2;
+        perimetro = 2*base+2*altura;
+        area = altura*base;
+        System.out.println("(" + r2.x1+"," + r2.y1+")" + "("+r2.x2+","+r2.y2+")" + " el périmetro es " + perimetro + " el area es " + area);
     }
 }
