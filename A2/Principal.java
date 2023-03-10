3f2cbfec7de1c769f04565ee43b5361f5dfca0a3
pokB01 <pokB01@x.y.z>
2023-03-10 13:14:51 +0100

    Creadas personas y sus mensajes

--- A2/Principal.java
+++ A2/Principal.java
@@ -1,7 +1,36 @@
 package A2;
 
 public class Principal {
-    Persona p1 = new Persona();   
-    Persona p2 = new Persona();   
+public static void main(String[] args) {
+    Persona p1 = new Persona();
+    Persona p2 = new Persona();
     
+    p1.nombre = "Juanlu";
+    p1.apellido= "Gómez García";
+    p1.dni="2438990L";
+    p1.edad=35;
+
+    p2.nombre = "Rodrigo";
+    p2.apellido = "Pérez";
+    p2.dni = "1337505S";
+    p2.edad = 15;
+
+    System.out.print(p1.nombre + " " + p1.apellido+ " con DNI " + p1.dni + " ");
+    if (p1.edad>= 18) {
+        System.out.println("es mayor de edad");
+    }
+    else{
+        System.out.println("no es mayor de edad");
+    }
+    
+    System.out.println();
+
+    System.out.print(p2.nombre + " " + p2.apellido+ " con DNI " + p2.dni + " ");
+    if (p2.edad>= 18) {
+        System.out.println("es mayor de edad");
+    }
+    else{
+        System.out.println("no es mayor de edad");
+    }
+}
 }
