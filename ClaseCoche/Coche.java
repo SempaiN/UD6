9a25cb439fbe657c6f6dab677acfb149711beed1
Sempain <trollnacho.np@gmail.com>
2023-03-13 16:26:44 +0100

    Creados los atributos del coche

--- ClaseCoche/Coche.java
+++ ClaseCoche/Coche.java
@@ -1,1 +1,15 @@
 
+package ClaseCoche;
+
+/**
+ * Ignacio Pérez Pose
+ */
+public class Coche {
+    //Atributos
+    private String modelo,color,tipoPintura,matricula;
+    private int añoFabricacion;
+    private enum tipoCoche {mini,utilitario,familiar,deportivo};
+    private enum modalidadSeguro{terceros,todoRiesgo};
+
+    
+}
