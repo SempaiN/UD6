ee3cb6887743a0ab23edd44c00e8aea9b8d81d97
pokB01 <pokB01@x.y.z>
2023-03-13 08:55:08 +0100

    Creado el ctor del articulo

--- B4/Articulo.java
+++ B4/Articulo.java
@@ -1,5 +1,14 @@
 package B4;
 
 public class Articulo {
+    String nombre;
+    double precioConIVA,precioSinIVA;
+    double iva = 1.21;
+    int cuantosQuedan;
+    public Articulo(String Nom,double precio,int cuantos){
+        this.nombre = Nom;
+        this.precioSinIVA = precio;
+        this.cuantosQuedan = cuantos;
+    }
     
 }
