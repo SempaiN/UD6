c7326fa8f858edde16d9681bcb7b15e5837f6cb3
Sempain <trollnacho.np@gmail.com>
2023-03-13 16:18:54 +0100

    Funcionalidad completa

--- ClaseAsignatura/Principal.java
+++ ClaseAsignatura/Principal.java
@@ -2,6 +2,10 @@ package ClaseAsignatura;
 
 public class Principal {
     public static void main(String[] args) {
-        
+        clase c1 = new clase("Historia", 2245, 2);
+        clase c2 = new clase("Castellano", 4214, 7);
+        c1.imprimeCurso();
+        System.out.println();
+        c2.imprimeCurso();
     }
 }
