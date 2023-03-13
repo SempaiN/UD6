a0fd7bafd535fb078049a0197c9c43f6e1ee70bd
pokB01 <pokB01@x.y.z>
2023-03-13 08:39:43 +0100

    Creado el ctor de rectangulo

--- B3/Rectangulo.java
+++ B3/Rectangulo.java
@@ -1,5 +1,11 @@
 package B3;
 
 public class Rectangulo {
-    
+    int x1,x2,y1,y2;
+    public Rectangulo(int X1,int Y1,int X2,int Y2){
+        this.x1 = X1;
+        this.x2 = X2;
+        this.y1 = Y1;
+        this.y2 = Y2;
+    }
 }
