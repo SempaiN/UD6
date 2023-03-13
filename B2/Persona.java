7eb22a26f1dc1565cbe3c6f91312518511ca37b2
pokB01 <pokB01@x.y.z>
2023-03-13 08:29:10 +0100

    Creado el ctor de personas

--- B2/Persona.java
+++ B2/Persona.java
@@ -1,5 +1,12 @@
 package B2;
 
 public class Persona {
-    
+    String dni,nombre,apellidos;
+    int edad;
+    public Persona(String nom,String Ape,String nif,int edat){
+        this.dni = nif;
+        this.nombre = nom;
+        this.apellidos = Ape;
+        this.edad = edat;
+    }
 }
