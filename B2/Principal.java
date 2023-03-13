1c592deea105a6f9d8bd73b81e349c36d6b3c3d7
pokB01 <pokB01@x.y.z>
2023-03-13 08:33:36 +0100

    Creadas personas con ctor

--- B2/Principal.java
+++ B2/Principal.java
@@ -2,6 +2,7 @@ package B2;
 
 public class Principal {
     public static void main(String[] args) {
-        
+        Persona carlos = new Persona("Carlos", "Rua", "24328990L", 22);
+        Persona julia = new Persona("Julia", "García", "44234234P", 44);
     }
 }
