101fb3f7d4e9e534e57a22d97903de62aebfd3b7
pokB01 <pokB01@x.y.z>
2023-03-13 09:05:32 +0100

    Funcionalidad completa

--- B4/Principal.java
+++ B4/Principal.java
@@ -2,6 +2,14 @@ package B4;
 
 public class Principal {
     public static void main(String[] args) {
-        
+        Articulo a1 = new Articulo("Pijama", 10, 44);
+        a1.precioConIVA = a1.precioSinIVA*a1.iva;
+        if (a1.precioSinIVA > 0 && a1.cuantosQuedan > 0) {
+            System.out.println(a1.nombre + " - Precio:" + a1.precioSinIVA + " - IVA:"+a1.iva+"% - PVP:" + a1.precioConIVA );
+
+        }
+        else {
+            System.out.println("ERROR");
+        }
     }
 }
