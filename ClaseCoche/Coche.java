6e2fa7465e14240cafb6238e160451d88aa395da
Sempain <trollnacho.np@gmail.com>
2023-03-13 16:33:15 +0100

    Creados setters y getters

--- ClaseCoche/Coche.java
+++ ClaseCoche/Coche.java
@@ -11,5 +11,39 @@ public class Coche {
     private enum tipoCoche {mini,utilitario,familiar,deportivo};
     private enum modalidadSeguro{terceros,todoRiesgo};
 
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    public String getModelo(){
+        return this.modelo;
+    }
     
+    public void setColor(String color){
+        this.color = color;
+    }
+    public String getColor(){
+        return this.color;
+    }
+    
+    public void setTipoPintura(String pintura){
+        this.tipoPintura = pintura;
+    }
+    public String getTipoPintura(){
+        return this.tipoPintura;
+    }
+    
+    public void setMatricula(String matricula){
+        this.matricula = matricula;
+    }
+    public String getMatricula(){
+        return this.matricula;
+    }
+
+    public void setAñoFabric(int año){
+        this.añoFabricacion = año;
+    }
+    public int getAñoFabric(){
+        return this.añoFabricacion;
+    }
+
 }
