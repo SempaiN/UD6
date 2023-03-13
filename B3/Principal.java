52eb9a2e9b5f080e1a69f5ffdea22bd2a49ae979
pokB01 <pokB01@x.y.z>
2023-03-13 08:47:17 +0100

    Creado todo lo referente a rectangulo

--- B3/Principal.java
+++ B3/Principal.java
@@ -2,6 +2,33 @@ package B3;
 
 public class Principal {
     public static void main(String[] args) {
+        Rectangulo r1 = new Rectangulo(0, 0, 5, 5);
+        Rectangulo r2 = new Rectangulo(7, 9, 2, 2);
         
+        double altura = r1.y2-r1.y1;
+        double base = r1.x2-r1.x1;
+        double perimetro = 2*base+2*altura;
+        double area = altura*base;
+        
+        if (r1.x1 < r1.x2 && r1.y1 < r1.y2) {
+            System.out.println("(" + r1.x1+"," + r1.y1+")" + "("+r1.x2+","+r1.y2+")" + " el périmetro es " + perimetro + " el area es " + area);
+        }
+        else {
+            System.out.println( "ERROR");
+        }
+        
+        System.out.println();
+        
+        altura = r2.y2-r2.y1;
+        base = r2.x2-r2.x1;
+        perimetro = 2*base+2*altura;
+        area = altura*base;
+
+        if (r2.x1 < r2.x2 && r2.y1 < r2.y2) {
+            System.out.println("(" + r2.x1+"," + r2.y1+")" + "("+r2.x2+","+r2.y2+")" + " el périmetro es " + perimetro + " el area es " + area);
+        }
+        else {
+            System.out.println( "ERROR");
+        }
     }
 }
--- B3/Rectangulo.java
+++ B3/Rectangulo.java
@@ -2,6 +2,7 @@ package B3;
 
 public class Rectangulo {
     int x1,x2,y1,y2;
+    
     public Rectangulo(int X1,int Y1,int X2,int Y2){
         this.x1 = X1;
         this.x2 = X2;
