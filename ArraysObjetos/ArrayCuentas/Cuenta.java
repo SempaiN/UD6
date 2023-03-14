afd68aa59f89ae9a74f644f30520fdd84bb58b9d
Sempain <trollnacho.np@gmail.com>
2023-03-14 22:42:26 +0100

    Creados archivos de aray cuentas

---
+++ ArraysObjetos/ArrayCuentas/Cuenta.java
@@ -0,0 +1,5 @@
+package ArrayCuentas;
+
+public class Cuenta {
+    
+}
---
+++ ArraysObjetos/ArrayCuentas/Principal.java
@@ -0,0 +1,7 @@
+package ArrayCuentas;
+
+public class Principal {
+    public static void main(String[] args) {
+        
+    }
+}
