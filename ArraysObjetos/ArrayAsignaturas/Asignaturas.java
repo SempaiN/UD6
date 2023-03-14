ea1a6bdb46f6e03ad8a132197d884a57bd5048fb
pokB01 <pokB01@x.y.z>
2023-03-14 13:43:00 +0100

    Creados los métodos para las asignaturas

--- ArraysObjetos/ArrayAsignaturas/Asignaturas.java
+++ ArraysObjetos/ArrayAsignaturas/Asignaturas.java
@@ -1,9 +1,40 @@
 package ArrayAsignaturas;
 
 /**
- * Asignaturas
+ * Ignacio Pérez Pose
  */
 public class Asignaturas {
-    private int h;
-    
+    private String nombre;
+    private int codigoNumerico,curso;
+
+    public Asignaturas(String nombre,int codigoNumerico, int curso){
+        this.nombre = nombre;
+        this.codigoNumerico = codigoNumerico;
+        this.curso = curso;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setCodigoNumerico(int codigoNumerico) {
+        this.codigoNumerico = codigoNumerico;
+    }
+    public int getCodigoNumerico() {
+        return codigoNumerico;
+    }
+
+    public void setCurso(int curso) {
+        this.curso = curso;
+    }
+    public int getCurso() {
+        return curso;
+    }
+
+    public void imprimeCurso(){
+        System.out.println(this.nombre + " código " + this.codigoNumerico + " clase " + this.curso);
+    }
 }
