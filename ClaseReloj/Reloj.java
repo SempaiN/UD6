e97065333c39ec82673c541a7c6c894ff621d28c
pokB01 <pokB01@x.y.z>
2023-03-14 12:00:28 +0100

    Creados getters setters y ctor de reloj

--- ClaseReloj/Reloj.java
+++ ClaseReloj/Reloj.java
@@ -1,5 +1,38 @@
 package ClaseReloj;
 
 public class Reloj {
-    
+    private int hora,minuto,segundo;
+
+    public Reloj(int hora,int minuto,int segundo){
+        this.hora = hora;
+        this.minuto = minuto;
+        this.segundo = segundo;
+    }
+    public Reloj(){
+        this.hora = 0;
+        this.minuto = 0;
+        this.segundo = 0;
+    }
+
+    public void setHora(int hora){
+        this.hora = hora;
+    }
+    public int getHora(){
+        return this.hora;   
+    }
+
+    public void setMinuto(int minuto){
+        this.minuto = minuto;
+    }
+    public int getMinuto(){
+        return this.minuto;   
+    }
+
+    public void setSegundo(int segundo){
+        this.segundo = segundo;
+    }
+    public int getSegundo(){
+        return this.segundo;   
+    }
+
 }
