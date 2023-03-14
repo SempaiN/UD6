b321b5a776b980cca5c91dba7f20a4f782666ba4
pokB01 <pokB01@x.y.z>
2023-03-14 14:13:22 +0100

    creados los atributos del coche

--- ArraysObjetos/ArrayCoches/Coche.java
+++ ArraysObjetos/ArrayCoches/Coche.java
@@ -1,5 +1,10 @@
 package ArrayCoches;
 
 public class Coche {
+    private enum tipoCoche{mini,utilitario,familiar,deportivo};
+    private enum tipoSeguro{tercero,todoRiesgo};
+    private String modelo,color,matricula;
+    private int añoFabricacion;
+    private boolean esMetalizado;
     
 }
