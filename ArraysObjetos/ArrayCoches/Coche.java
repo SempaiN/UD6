b0e39df0c8d6db4a0b90d0fd0d987e79c517f9cf
pokB01 <pokB01@x.y.z>
2023-03-14 14:55:42 +0100

    r

--- ArraysObjetos/ArrayCoches/Coche.java
+++ ArraysObjetos/ArrayCoches/Coche.java
@@ -1,8 +1,8 @@
 package ArrayCoches;
 
 public class Coche {
-    private enum tipoCoche{mini,utilitario,familiar,deportivo};
-    private enum tipoSeguro{tercero,todoRiesgo};
+    public enum tipoCoche{mini,utilitario,familiar,deportivo};
+    public enum tipoSeguro{tercero,todoRiesgo};
     private String modelo,color,matricula;
     private int añoFabricacion;
     private boolean esMetalizado;
