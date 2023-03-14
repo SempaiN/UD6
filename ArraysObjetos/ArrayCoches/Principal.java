11ab599d9b11cd4f469e5162c7c2a800ac1c5ed0
pokB01 <pokB01@x.y.z>
2023-03-14 14:55:40 +0100

    11

--- ArraysObjetos/ArrayCoches/Principal.java
+++ ArraysObjetos/ArrayCoches/Principal.java
@@ -1,7 +1,25 @@
 package ArrayCoches;
 
+import java.util.Scanner;
+
 public class Principal {
+    public static Scanner lec = new Scanner(System.in);
     public static void main(String[] args) {
+    Coche coches [] = new Coche[3];
+    String modelo,color,matricula;
+    int añoFabricacion;
+    boolean esMetalizado;
+    Coche.tipoCoche tipoC;
+    Coche.tipoSeguro tipoS;
+    for (int i = 0; i < coches.length; i++) {
+        System.out.println("Dame el modelo del coche");
+        modelo = lec.next();
+        coches[i].setModelo(modelo);
+        System.out.println("Dame el color del coche");
+        coches[i].setColor(lec.next());
         
+    }
+
+
     }
 }
