04d24c475be1ca64fbe10b3755380d098c6f30c8
pokB01 <pokB01@x.y.z>
2023-03-14 12:50:39 +0100

    Pruebas en el main

--- ClaseReloj/Principal.java
+++ ClaseReloj/Principal.java
@@ -2,7 +2,11 @@ package ClaseReloj;
 
 public class Principal {
     public static void main(String[] args) {
-        Reloj r1 = new Reloj(13, 0, 0);
+        Reloj r1 = new Reloj(23, 59, 59);
+        
+        r1.imprimeHora();
+        r1.imprimeHora12();
+        r1.tick();
         r1.imprimeHora();
         r1.imprimeHora12();
 
