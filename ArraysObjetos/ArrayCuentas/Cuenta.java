1130b3cf90db25683551d7164d7aed693f76e751
Sempain <trollnacho.np@gmail.com>
2023-03-14 22:47:26 +0100

    Creados getters setters y ctor

--- ArraysObjetos/ArrayCuentas/Cuenta.java
+++ ArraysObjetos/ArrayCuentas/Cuenta.java
@@ -1,5 +1,30 @@
 package ArrayCuentas;
 
 public class Cuenta {
-    
+    private int numCuenta;
+    private double saldo;
+
+    public Cuenta(int ncuenta,double dinero){
+        setNumCuenta(ncuenta);
+        setSaldo(dinero);
+    }
+
+    public void setNumCuenta(int ncuenta){
+        this.numCuenta = ncuenta;
+    }
+    public void setSaldo(double dinero){
+        if (dinero > 0 ) {
+            this.saldo = dinero;
+        }
+        else{
+            System.out.println("Cantidad Erronea");
+        }
+    }
+
+    public int getNumCuenta(){
+        return this.numCuenta;
+    }
+    public double getSaldo(){
+        return this.saldo;
+    }
 }
