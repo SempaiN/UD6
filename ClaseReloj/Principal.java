6de82fe8049d29763952609b744d95e26e15d456
pokB01 <pokB01@x.y.z>
2023-03-20 08:16:43 +0100

    Cambio hecho en cuenta

--- ClaseReloj/Principal.java
+++ ClaseReloj/Principal.java
@@ -1,15 +1,8 @@
 package ClaseReloj;
 
 public class Principal {
-    public static void main(String[] args) {
-        Reloj r1 = new Reloj(22, 20, 59);
-        System.out.println();
-        r1.imprimeHora();
-        r1.imprimeHora12();
-        r1.tick();
-        System.out.println();
-        r1.imprimeHora();
-        r1.imprimeHora12();
-
-    }   
+    public static void main(String[] args) {    
+        Reloj relojes [] = new Reloj[5];
+        relojes[0] = new Reloj(12, 15, 21)
+    }  
 }
