474806639cbe83a3c8b63b75e640f1f91beab201
pokB01 <pokB01@x.y.z>
2023-03-20 08:25:36 +0100

    reloje

--- ArraysObjetos/ArrayReloj/Principal.java
+++ ArraysObjetos/ArrayReloj/Principal.java
@@ -1,7 +1,18 @@
 package ArrayReloj;
 
 public class Principal {
+    public static Reloj items[] = new Reloj[5];
     public static void main(String[] args) {
+
+        
+        items[0] = new Reloj(15, 16, 18); 
+        items[1] = new Reloj(21, 21, 21);
+        items[2] = new Reloj(10, 59, 59);
+        items[3]= new Reloj(23, 0, 59);
+        items[4] = new Reloj(0, 0, 1);
+         
         
     }
+
+
 }
