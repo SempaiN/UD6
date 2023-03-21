28b9a61eae13fac94787175c969b581c5c3fd900
pokB01 <pokB01@x.y.z>
2023-03-21 13:23:24 +0100

    App banco con objetos

---
+++ AppBanco/Cuenta.java
@@ -0,0 +1,5 @@
+package AppBanco;
+
+public class Cuenta {
+    
+}
---
+++ AppBanco/Principal.java
@@ -0,0 +1,8 @@
+package AppBanco;
+
+public class Principal {
+    public static void main(String[] args) {
+        
+        
+    }
+}
