37fab9d707937123262cf8445a47e780b8f757fb
pokB01 <pokB01@x.y.z>
2023-03-21 13:53:32 +0100

    Setters y getters

--- AppBanco/Cuenta.java
+++ AppBanco/Cuenta.java
@@ -1,5 +1,24 @@
 package AppBanco;
 
 public class Cuenta {
+    private String Nombre;
+    private double saldo;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
     
 }
